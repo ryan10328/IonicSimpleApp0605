38da66857e0137f661b7ff1289bc57ac6ff2d198
ryan10328 <ryan10328@gmail.com>
2017-06-05 11:39:29 +0800

    3. 新增 List 元件，並做出一點變化

--- src/pages/hello/hello.ts
+++ src/pages/hello/hello.ts
@@ -14,6 +14,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HelloPage {
 
+  students: any[] = [{
+    name: 'Ryan'
+  },
+  {
+    name: 'Jeff'
+  }];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
