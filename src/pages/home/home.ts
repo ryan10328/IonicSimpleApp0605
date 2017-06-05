d67bb41bc5d4936869198e173099a300e84d4d5b
ryan10328 <ryan10328@gmail.com>
2017-06-05 11:45:51 +0800

    4. 使用 Alert 元件

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private alertCtrl: AlertController) {
 
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Hello Ionic Alert',
+      message: 'This is a simple alert'
+    });
+    alert.present();
   }
 
 }
